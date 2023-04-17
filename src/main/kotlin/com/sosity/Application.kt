b701ede0f201af3c5a8bc651faf93afa608916d7
mmt10741 <aman.kapoor@go-mmt.com>
2023-04-17 21:46:59 +0530

    enbeddded

--- src/main/kotlin/com/sosity/Application.kt
+++ src/main/kotlin/com/sosity/Application.kt
@@ -1,11 +1,13 @@
 package com.sosity
 
-import io.ktor.server.application.*
 import com.sosity.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main(args: Array<String>): Unit =
-    EngineMain.main(args)
+fun main(args: Array<String>) =
+    embeddedServer(Netty, port = System.getProperty("PORT").toIntOrNull()?:8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
