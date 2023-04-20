8ced5183cbc1a509fc26243e5a8c1237c46f83c7
mmt10741 <aman.kapoor@go-mmt.com>
2023-04-20 22:16:31 +0530

    mongo url from environment fix

--- src/main/kotlin/com/sosity/database/Db.kt
+++ src/main/kotlin/com/sosity/database/Db.kt
@@ -6,8 +6,9 @@ import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 import org.litote.kmongo.util.ObjectMappingConfiguration
 import org.slf4j.LoggerFactory
+import java.util.logging.Logger
 
-val connectionString: String = System.getProperty("MONGO_DB_URL", "mongodb://localhost")
+val connectionString: String = System.getenv("MONGO_DB_URL") ?: "mongodb://localhost"
 val client = KMongo.createClient(connectionString).coroutine //use coroutine extension
 val database = client.getDatabase("societies")//normal java driver usage
 
@@ -17,6 +18,7 @@ object Db {
         val loggerContext = LoggerFactory.getILoggerFactory() as LoggerContext
         val rootLogger = loggerContext.getLogger("org.mongodb.driver")
         rootLogger.level = Level.OFF
+        Logger.getGlobal().info(connectionString)
         database
         ObjectMappingConfiguration.serializeNull = false
     }
